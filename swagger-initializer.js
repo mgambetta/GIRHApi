fffaa6faa51029202846916d3e6bd7e6bc19bf4a
mgambetta <110406748+mgambetta@users.noreply.github.com>
2022-08-01 12:23:19 -0300

    Update

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -7,8 +7,8 @@ window.onload = function() {
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
+      SwaggerUIBundle.presets.apis
+      
     ],
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
