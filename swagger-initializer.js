f9ae1e901440a94e8be1326dec925601d3edb977
mgambetta <110406748+mgambetta@users.noreply.github.com>
2022-09-08 19:46:18 -0300

    5

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -4,7 +4,6 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "openapi.json",
-    defaultModelsExpandDepth: -1,
     dom_id: '#swagger-ui',
     deepLinking: false,
     presets: [
